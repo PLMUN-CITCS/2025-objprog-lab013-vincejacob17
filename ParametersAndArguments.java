246f2a9a34f647905c6251397b9ad41eea51b37c
Vince Adriel Jacob <jacobvinceadriel_bscs@plmun.edu.ph>
2025-02-27 17:36:40 +0800

    Submitting OBJPROG Week 05 - Laboratory # 13

---
+++ ParametersAndArguments.java
@@ -0,0 +1,39 @@
+public class ParametersAndArguments {
+    public static void main(String[] args) {
+public class ParametersAndArguments {
+    public static void main(String[] args) {
+
+    }
+}
+public class ParametersAndArguments {
+    //... (main method)...
+
+    public static int calculateArea(int width, int height) {
+        int area = width * height;
+        return area;
+    }
+}
+public class ParametersAndArguments {
+    public static void main(String[] args) {
+        int roomWidth = 10;
+        int roomHeight = 15;
+    }
+    //... (calculateArea method)...
+}
+public class ParametersAndArguments {
+    public static void main(String[] args) {
+        //... (variable declarations)...
+        int area = calculateArea(roomWidth, roomHeight);
+    }
+    //... (calculateArea method)...
+}
+public class ParametersAndArguments {
+    public static void main(String[] args) {
+        //... (other code in main)...
+        System.out.println("The calculated area is: " + area);
+    }
+    //... (calculateArea method)...
+}
+
+    }
+}
